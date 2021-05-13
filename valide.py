3d91b03b19e8ef846427f6612cef87a0e0a20318
Evraa <evramyousef@gmail.com>
2021-05-13 05:38:47 +0200

    validate double notations

--- valide.py
+++ valide.py
@@ -2,7 +2,6 @@ import re
 from collections import deque
  
 def split(word):
-    word = word.replace(" ","")
     return list(word)
     
 def rb_dont_match(list_of_ch):
@@ -13,11 +12,11 @@ def rb_dont_match(list_of_ch):
     for ch in list_of_ch:
         if ch == "(":
             rb_stack.append("(")
-            print (rb_stack)
-        if ch == ")":
+            # print (rb_stack)
+        elif ch == ")":
             if len(rb_stack)==0: return True
             rb_stack.popleft()
-            print (rb_stack)
+            # print (rb_stack)
     
     if len(rb_stack)>=1: return True
     return False
@@ -27,15 +26,48 @@ def doublers_found(txt):
     '''
         check for these patterns |* *| || ** ..etc.
     '''
-    txt = txt.replace(" ","")
-    res = re.finditer("[|*]", txt)
+    res = re.finditer("[|*()]", txt)
     
     prev = -1
+    prev_ch = None
     for i,r in enumerate(res):
+        #check for start with (
+        if r.group() == '(' and i == 0:
+            prev = r.start()
+            prev_ch = r.group()
+            continue
+
         ind = r.start()
-        if ind-prev <= 1: return True
+        #if two concecutives are notations..check for valid cases or terminate
+        if ind-prev <= 1: 
+            valid = False
+            #check for valid cases
+            current_ch = r.group()
+            if prev_ch == '*' and current_ch ==")":
+                valid = True
+            if prev_ch == ')' and current_ch =="*":
+                valid = True
+            if prev_ch == '*' and current_ch =="|":
+                valid = True
+            if prev_ch == '*' and current_ch =="(":
+                valid = True
+            if prev_ch == '|' and current_ch =="(":
+                valid = True
+            if prev_ch == ')' and current_ch =="|":
+                valid = True
+            if prev_ch == '(' and current_ch =="(":
+                valid = True
+            if prev_ch == ')' and current_ch ==")":
+                valid = True
+            if prev_ch == ')' and current_ch =="(":
+                valid = True
+            if not valid: return True
+
+
         prev = ind
-    
+        prev_ch = r.group()
+        
+
     return False
 
 
@@ -47,6 +79,11 @@ def validate(txt):
         + () lengths mathces
         + () close to eachothers
     '''
+
+    #remove spaces
+    txt = txt.replace(" ","")
+    #empty?
+    if txt == "": return False
 
     #check for digits
     if re.search(r"\d",txt): return False
