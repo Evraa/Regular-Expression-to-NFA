02f17097c1a86ac0499c9e1d436af96cc0b919b2
Evraa <evramyousef@gmail.com>
2021-05-14 23:48:04 +0200

    concatenation

--- state.py
+++ state.py
@@ -16,34 +16,72 @@ class Node:
 
 
 
-main_list = []
-eps = 'e'
-
 
 def init_nodes():
-    s0 = Node(id=0, start=True, left_rb=True, to_node=1, char_to=eps)
+    global i, main_list, eps, last_node
+    s0 = Node(id=i, start=True, left_rb=True, to_node=1, char_to=eps)
+    i+=1
     main_list.append(s0)
-    s1 = Node(id=1)
+    s1 = Node(id=i)
+    i+=1
     main_list.append(s1)
+    last_node = s1
     
 
 def print_main_list(short=True):
+
+    global main_list
+
     for n in main_list:
         if short:
             print (f'Node: {n.id}\t\tto node:{n.to_node}\tchar:{n.char_to}')
         else:
             print (f'Node: {n.id}\t\tto node:{n.to_node}\tchar:{n.char_to}\t\tStart:{n.start}\tEnd:{n.end}\tLeft RB:{n.left_rb}\tRight RB:{n.right_rb}\tMain OR:{n.main_or}\tOpen OR:{n.open_or}\t')
 
+    print ("##########EOF############\n")
+def concatenate(char_to):
+    global i, main_list, eps, last_node
+    
+    last_node.to_node = i
+    last_node.char_to = char_to
+    
+    si = Node(id=i, to_node=i+1, char_to=eps)
+    i+=1
+    main_list.append(si)
+    si = Node(id=i)
+    i+=1
+    main_list.append(si)
+    last_node = si
+    
+
+
+
 def state(txt):
+    global i, main_list, eps, last_node
+    
     '''
 
     '''
-
+    print(txt)
     init_nodes()
     print_main_list()
+    for ch in txt:
+        if ch == "(":
+            pass
+        elif ch == ")":
+            pass
+        else:
+            concatenate(ch)
+        print_main_list()
+
 
 
 if __name__ == "__main__":
+    #Globals
+    main_list = []
+    i = 0
+    eps = 'e'
+    last_node = None
     # txt = str(input("Insert RE:\n"))
     # valide.validate(txt)
     # txt = parse.parse(txt)
