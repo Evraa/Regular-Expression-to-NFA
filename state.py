a0f77e974247c3ce86ee2b9befdf132e3c57783c
Evraa <evramyousef@gmail.com>
2021-05-15 17:59:54 +0200

    removed last_node, it's already exist in the last node of main list

--- state.py
+++ state.py
@@ -32,7 +32,7 @@ class Node:
         self.children = copy.deepcopy(new_children)
 
 def init_nodes():
-    global i, main_list, eps, last_node, first_node
+    global i, main_list, eps,first_node
     first_node = Node(id=i, start=True, left_rb=True)
     i+=1
     main_list.append(first_node)
@@ -40,7 +40,7 @@ def init_nodes():
     i+=1
     main_list.append(s1)
     first_node.add_child(s1, eps)
-    last_node = s1
+    # main_list[-1] = s1
     
 
 def print_main_list(short=True):
@@ -60,58 +60,66 @@ def print_main_list(short=True):
 
 
 def concatenate(char_to):
-    global i, main_list, eps, last_node
+    global i, main_list, eps
     #fetch last node
     si = Node(id=i)
     i+=1
     main_list.append(si)
-    last_node.add_child(si, char_to)
+    main_list[-1].add_child(si, char_to)
     #create new empty one with eps.
     sj = Node(id=i)
     i+=1
     main_list.append(sj)
     si.add_child(sj, eps)
     
-    #pointer to last node.
-    last_node = sj
+    # #pointer to last node.
+    # main_list[-1] = sj
     
 def terminate():
-    last_node.right_rb = True #doen't matter btw.
-    last_node.end = True
+    main_list[-1].right_rb = True #doen't matter btw.
+    main_list[-1].end = True
 
 def left_bracket():
-    global i, main_list, eps, last_node
+    global i, main_list, eps
     
-    if last_node.left_rb:
+    if main_list[-1].left_rb:
         si = Node (id=i, left_rb=True) 
         main_list.append(si)
-        last_node = si
+        # main_list[-1] = si
 
-    last_node.left_rb = True
+    main_list[-1].left_rb = True
 
-def right_bracket():
-    last_node.right_rb = True
+def right_bracket(new_main_or):
+    global main_list,first_node,  eps, i
+    
+    if new_main_or is not None:
+        #there's a OR that we need to close
+        new_last = Node (id=i)
+
+
+    else:
+        main_list[-1].right_rb = True
 
 
 def asterisk(last_open_bracket):
-    global main_list,first_node, last_node, eps, i
+    global main_list,first_node, eps, i
     #check that last bracket open has only one child
 
-    #find last_node's dad
+    #find main_list[-1]'s dad
     last_dad = None
 
     for n in main_list:
         for c in n.children:
-            if c[0].id == last_node.id:
+            if c[0].id == main_list[-1].id:
                 last_dad = n
 
-    last_open_bracket.add_child(last_node, eps) 
+    last_open_bracket.add_child(main_list[-1], eps) 
    
     last_dad.add_child(last_open_bracket, eps)
 
 
 def shift_update_first():
-    global main_list, first_node, last_node, eps, i, spare_list
+    global main_list, first_node, eps, i, spare_list
     first_node.start = False
     s0 = Node(id=0, start=True, left_rb=False, main_or=True)
     s0.add_child(first_node, eps)
@@ -129,7 +137,7 @@ def shift_update_first():
     
 
 def shift_update(last_open_bracket):
-    global main_list, first_node, last_node, eps, i, spare_list
+    global main_list, first_node,  eps, i, spare_list
     
     s0 = Node(id=last_open_bracket.id, main_or=True)
     s0.add_child(last_open_bracket, eps)
@@ -164,20 +172,21 @@ def shift_update(last_open_bracket):
     
 
 def oring(last_open_bracket) -> None:
-    global main_list,first_node, last_node, eps, i
+    global main_list,first_node, eps, i
 
     #stuffing and shifting
-    last_node.open_or = True
+    main_list[-1].open_or = True
 
     if last_open_bracket == first_node: 
         new_main_or = shift_update_first()
     else:
         new_main_or = shift_update(last_open_bracket)
 
+
     new_last = Node (id=i)
     main_list[-1].add_child(new_last, eps)
 
-    last_node = new_last
+    main_list[-1] = new_last
 
     main_list.append(new_last)
     i += 1
@@ -185,7 +194,7 @@ def oring(last_open_bracket) -> None:
 
 
 def state(txt):
-    global i, main_list, eps, last_node, first_node
+    global i, main_list, eps, first_node
 
     rb_stack = deque()
     first_bracket = False
@@ -198,16 +207,16 @@ def state(txt):
                 rb_stack.append(first_node)
                 first_bracket = True
             else:
-                last_open_bracket = last_node
+                last_open_bracket = main_list[-1]
                 left_bracket()
-                rb_stack.append(last_node)
+                rb_stack.append(main_list[-1])
             
         elif ch == ")":
             if len(rb_stack)==1: #last one
                 #this is end (TERMINATE)
                 terminate()
             else:
-                right_bracket()
+                right_bracket(new_main_or)
                 rb_stack.pop()
                 last_open_bracket = rb_stack[-1]
 
@@ -229,7 +238,7 @@ if __name__ == "__main__":
     main_list = []
     i = 0
     eps = 'e'
-    last_node = None
+    # main_list[-1] = None
     first_node = None
     spare_list = []
     # txt = str(input("Insert RE:\n"))
