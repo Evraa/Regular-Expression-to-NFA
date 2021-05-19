f0650136c69b0970b653ab53376fc20a41a8d577
Evraa <evramyousef@gmail.com>
2021-05-19 19:12:38 +0200

    More into graphs

--- graph.py
+++ graph.py
@@ -3,35 +3,29 @@ from graphviz import Digraph
 
 
 def graph(main_list):
+    if len(main_list) == 0: return
 
     dot = Digraph(comment='NFA')
     dot  #doctest: +ELLIPSIS
-
+    dot.attr(rankdir="LR")
     for node in main_list:
         node_name = "node_"+str(node.id)
+        if node.end:
+            dot.attr('node', shape="doublecircle")
+        else:
+            dot.attr('node', shape="circle")
         dot.node(node_name,node_name)
+
+    dot.attr('node', shape="plaintext")
+    dot.node("start","start")
+
+    dot.edge(head_name="node_0", tail_name="start")
 
     for node in main_list:
         node_name = "node_"+str(node.id)
         for child in node.children:
             child_name = "node_"+str(child[0].id)
             dot.edge(node_name, child_name, label=child[1])
-            
-    print(dot.source) 
+
+    # print(dot.source) 
     dot.render('test-output/round-table.gv', view=True)
-
-# if __name__ == "__main__":
-
-#     dot = Digraph(comment='NFA')
-#     dot  #doctest: +ELLIPSIS
-
-#     dot.node('A', 'King Arthur')
-#     dot.node('B', 'Sir Bedevere the Wise')
-#     dot.node('L', 'Sir Lancelot the Brave')
-
-#     dot.edges(['AB', 'AL'])
-#     dot.edge('B', 'L', label="ec")
-
-#     print(dot.source) 
-
-#     dot.render('test-output/round-table.gv', view=True)
